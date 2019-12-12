86f7df591a5f27da6d7855eb0f9fe265491c54ce
Andrzej Holubowicz <andrzej.holubowicz@ig.com>
2019-12-12 13:02:19 +0100

    7

--- src/advent2019/7.clj
+++ src/advent2019/7.clj
@@ -1,8 +1,14 @@
 (require '[clojure.string :as str])
+(require '[clojure.math.combinatorics :as combo])
 
 (def in (slurp "src/advent2019/7.in"))
 
 (def numbers (vec (->> (str/split in #",") (map read-string))))
+;(def numbers [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])
+;(def numbers [3,23,3,24,1002,24,10,24,1002,23,-1,23,
+;              101,5,23,23,1,24,23,23,4,23,99,0,0))
+;(def numbers [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
+;              1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0))
 
 (defn make-computer [numbers input]
   {:memory (into {} (map-indexed #(vector %1 %2) numbers))
@@ -12,9 +18,10 @@
 (def computer (make-computer numbers [1]))
 
 (defn mem-value [memory mode pos]
-  (case mode
-    0 (memory (memory pos))
-    1 (memory pos)))
+  (let [result (case mode
+                 0 (memory (memory pos))
+                 1 (memory pos))]
+    (if (nil? result) 0 result)))
 
 (defn update-memory [{op :op modes :modes} pos c]
   (case op
@@ -76,8 +83,22 @@
             (calculate-pos instruction pos params-count c)
             (operation instruction pos c)))))))
 
-(def result (compute numbers [5]))
-(println (:output result))
-;14195011
+;(def result (compute numbers [3 0]))
+;(println (:output result))
 
-;(map second (sort-by first (:memory (compute numbers []))))
+(defn get-output [in]
+  (let [c in
+        res1 (compute numbers [(get c 0) 0])
+        ;_ (println (last res1))
+        res2 (compute numbers [(get c 1) (last (:output res1))])
+        res3 (compute numbers [(get c 2) (last (:output res2))])
+        res4 (compute numbers [(get c 3) (last (:output res3))])
+        res5 (compute numbers [(get c 4) (last (:output res4))])]
+    (last (:output res5))))
+
+;(get-output [0 1 2 3 4])
+;(let [c [0 1 2 3 4]]
+;  (println (get c 0))
+;  (compute numbers [(get c 0) 0]))
+
+(apply max (map get-output (combo/permutations [0 1 2 3 4])))
